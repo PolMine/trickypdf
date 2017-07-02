950cd672a9650a88f56edf497444a0e6a380e72a
Andreas Blaette <andreas.blaette@uni-due.de>
2017-07-02 16:20:36 +0200

    new tests for multi-column layouts

--- tests/testthat/test_trickypdf.R
+++ tests/testthat/test_trickypdf.R
@@ -12,7 +12,7 @@ test_that("restore_paragraphs", {
 })
 
 
-test_that("PDF", {
+test_that("one_column_layout", {
   expect_equal(
     {
       cdu_pdf <- system.file(package = "trickypdf", "extdata", "pdf", "cdu.pdf")
@@ -28,3 +28,42 @@ test_that("PDF", {
     296610
   )
 })
+
+test_that("two_column_layout_decolumnize", {
+  expect_equal(
+    {
+      plenaryprotocol <- system.file(package = "trickypdf", "extdata", "pdf", "18238.pdf")
+      P <- PDF$new(filename_pdf = plenaryprotocol, first = 5, last = 73)
+      P$add_box(c(left = 58, width = 480, top = 70, height = 705))
+      P$remove_unboxed_text_from_all_pages()
+      P$deviation <- 10L
+      P$decolumnize()
+      P$get_text_from_pages()
+      P$purge()
+      P$xmlify()
+      P$xml2html()
+      nchar(as.character(P$html))
+    },
+    373180
+  )
+})
+
+test_that("two_column_layout_boxes", {
+  expect_equal(
+    {
+      doc <- system.file(package = "trickypdf", "extdata", "pdf", "UN_GeneralAssembly_2016.pdf")
+      UN <- PDF$new(filename_pdf = doc)
+      UN$add_box(page = 1, box = c(top = 380, height = 250, left = 52, width = 255))
+      UN$add_box(page = 1, box = c(top = 232, height = 400, left = 303, width = 255), replace = FALSE)
+      UN$add_box(page = 2, box = c(top = 80, height = 595, left = 52, width = 255))
+      UN$add_box(page = 2, box = c(top = 80, height = 595, left = 303, width = 255), replace = FALSE)
+      UN$get_text_from_boxes(paragraphs = TRUE)
+      UN$xmlify()
+      UN$xml2html()
+      nchar(as.character(UN$html))
+      
+    },
+    8618
+  )
+})
+
